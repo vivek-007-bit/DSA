4fb30cccafe567c40c8fd587d12589c89df46060
Vivek Sharma <viveksharma40649@gmail.com>
2025-04-05 12:31:46 +0530

    Add files via upload

---
+++ linked_list/insertion/04_insert_after_node.c
@@ -0,0 +1,105 @@
+/*This program illustrates how to insert a node after a given node  of a linked list*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next; // self referncing structure
+};
+
+
+struct Node *insertAfterNode(struct Node *head, struct Node *prevNode){
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *p = head;
+    
+    int data = 0;
+    printf("\n...Enter the value of the Node: ");
+    scanf("%d", &data);
+
+    ptr->data =  data;
+
+    ptr->next = prevNode->next;
+    prevNode->next = ptr;
+
+    return head;
+}
+
+
+void linkedListTraversal(struct Node *ptr)
+{
+    int count = 0;
+    while (ptr != NULL)
+    {
+        count++;
+        printf("Element %d : %d\n", count, ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+// A function to display the visual representation of the linked list
+void display(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("|  %d  |", ptr->data);
+        if (ptr->next != NULL)
+        {
+            printf(" --> ");
+        }
+        ptr = ptr->next;
+    }
+    printf(" --> NULL\n");
+}
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+    struct Node *fifth;
+
+    struct Node *temp;
+
+    // Memory allocation for the nodes in the linked list
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+    fifth = (struct Node *)malloc(sizeof(struct Node));
+
+    // linking all the nodes
+    head->data = 55;
+    head->next = second;
+
+    second->data = 97;
+    second->next = third;
+
+    third->data = 5;
+    third->next = fourth;
+
+    fourth->data = 25;
+    fourth->next = fifth;
+
+    fifth->data = 46;
+    fifth->next = NULL;
+
+    printf("\n...The Elements of the linked list are...\n");
+    linkedListTraversal(head);
+
+    printf("\n... Insertion...\n");
+    head = insertAfterNode(head, third);
+    printf("\n");
+    display(head);
+
+    // freeing all the nodes
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return 0;
+}
