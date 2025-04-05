62f46dfc9eef395dbe0cf6f9ea471f9786d8de3f
Vivek Sharma <viveksharma40649@gmail.com>
2025-04-05 12:37:18 +0530

    Create 01_linear_search.c

---
+++ searching_algorithm/01_linear_search.c
@@ -0,0 +1,43 @@
+// this program only works for distinct elements
+#include <stdio.h>
+
+int linearSearch(int arr[], int size)
+{
+    int element;
+    printf("Enter the Element to be searched: ");
+    scanf("%d", &element);
+
+    // searching begins
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == element)
+        {
+            printf("The Element %d was found at index : %d", element, i);
+            return i;
+        }
+    }
+    printf("The Element %d was not found!", element);
+    return 0;
+}
+
+void traversal(int arr[], int size)
+{
+    printf("...Array elements...\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    int arr[] = {65, 36, 43, 89, 43, 67, 233, 67, 2, 3, 3};
+    int size = sizeof(arr) / sizeof(int);
+
+    traversal(arr, size);
+
+    int searchIndex = linearSearch(arr, size);
+    return 0;
+}
