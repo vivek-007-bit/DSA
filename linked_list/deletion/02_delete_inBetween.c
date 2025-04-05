4fddd19b61c1ffcbddc3e9f76e0e2d7e890c597f
Vivek Sharma <viveksharma40649@gmail.com>
2025-04-05 12:33:20 +0530

    Add files via upload

---
+++ linked_list/deletion/02_delete_inBetween.c
@@ -0,0 +1,108 @@
+/*This program illustrates how to delete a node in between  linked list or at a given index*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next; // self referncing structure
+};
+
+
+struct Node *deleteAtIndex(struct Node *head){
+    struct Node *p = head;
+    struct Node *q = head->next;
+
+    int index = 0;
+    printf("Enter the index of the node to be deleted\n");
+    scanf("%d", &index);
+
+    for (int i = 0; i < index-1; i++)
+    {
+       p = p->next;
+       q = q->next;
+    }
+    
+    p->next = q->next;
+    free(q);
+    return head;
+}
+
+
+void linkedListTraversal(struct Node *ptr)
+{
+    int count = 0;
+    while (ptr != NULL)
+    {
+        count++;
+        printf("Element %d : %d\n", count, ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+// A function to display the visual representation of the linked list
+void display(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("|  %d  |", ptr->data);
+        if (ptr->next != NULL)
+        {
+            printf(" --> ");
+        }
+        ptr = ptr->next;
+    }
+    printf(" --> NULL\n");
+}
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+    struct Node *fifth;
+
+    struct Node *temp;
+
+    // Memory allocation for the nodes in the linked list
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+    fifth = (struct Node *)malloc(sizeof(struct Node));
+
+    // linking all the nodes
+    head->data = 55;
+    head->next = second;
+
+    second->data = 97;
+    second->next = third;
+
+    third->data = 5;
+    third->next = fourth;
+
+    fourth->data = 25;
+    fourth->next = fifth;
+
+    fifth->data = 46;
+    fifth->next = NULL;
+
+    printf("\n...The Elements of the linked list are...\n");
+    linkedListTraversal(head);
+
+    printf("\n... Deleting at index...\n");
+    head = deleteAtIndex(head);
+    printf("\n");
+    display(head);
+
+    // freeing all the nodes
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return 0;
+}
---
+++ linked_list/deletion/04_delete_after_node.c
@@ -0,0 +1,115 @@
+/*This program illustrates how to delete a node with a given value of a linked list*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next; // self referncing structure
+};
+
+
+struct Node *deleteNode(struct Node *head){
+    struct Node *p = head;
+    struct Node *q = head->next;
+
+    int value = 0;
+    printf("Enter the value of gthe node to be deleted\n");
+    scanf("%d", &value);
+
+    while (q->data != value && q->next != NULL)
+    {
+        p = p->next;
+        q = q->next;
+    }
+    
+    if(q->data == value){
+        p->next = q->next;
+        free(q);
+    }
+
+    else{
+        printf("Value not found\n");
+    }
+
+    return head;
+}
+
+
+void linkedListTraversal(struct Node *ptr)
+{
+    int count = 0;
+    while (ptr != NULL)
+    {
+        count++;
+        printf("Element %d : %d\n", count, ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+// A function to display the visual representation of the linked list
+void display(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("|  %d  |", ptr->data);
+        if (ptr->next != NULL)
+        {
+            printf(" --> ");
+        }
+        ptr = ptr->next;
+    }
+    printf(" --> NULL\n");
+}
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+    struct Node *fifth;
+
+    struct Node *temp;
+
+    // Memory allocation for the nodes in the linked list
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+    fifth = (struct Node *)malloc(sizeof(struct Node));
+
+    // linking all the nodes
+    head->data = 55;
+    head->next = second;
+
+    second->data = 97;
+    second->next = third;
+
+    third->data = 5;
+    third->next = fourth;
+
+    fourth->data = 25;
+    fourth->next = fifth;
+
+    fifth->data = 46;
+    fifth->next = NULL;
+
+    printf("\n...The Elements of the linked list are...\n");
+    linkedListTraversal(head);
+
+    printf("\n... Deleting a node with a given value...\n");
+    head = deleteNode(head);
+    printf("\n");
+    display(head);
+
+    // freeing all the nodes
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return 0;
+}
